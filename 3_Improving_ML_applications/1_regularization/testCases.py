13b87f966184527416947678e1dacf7506b5f72e
Levin Mueller <muellerlevin@gmx.de>
2022-11-30 09:55:22 +0100

    exercise 3

---
+++ 3_Improving_ML_applications/1_regularization/testCases.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+def compute_cost_with_regularization_test_case():
+    np.random.seed(1)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    W1 = np.random.randn(2, 3)
+    b1 = np.random.randn(2, 1)
+    W2 = np.random.randn(3, 2)
+    b2 = np.random.randn(3, 1)
+    W3 = np.random.randn(1, 3)
+    b3 = np.random.randn(1, 1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3}
+    a3 = np.array([[ 0.40682402,  0.01629284,  0.16722898,  0.10118111,  0.40682402]])
+    return a3, Y_assess, parameters
+
+def backward_propagation_with_regularization_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    # (A1, W1, A2, W2, A3, W3)
+    # (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+    cache = (
+  np.array([[ 0.        ,  3.32524635,  2.13994541,  2.60700654,  0.        ],
+         [ 0.        ,  4.1600994 ,  0.79051021,  1.46493512,  0.        ]]),
+  np.array([[-1.09989127, -0.17242821, -0.87785842],
+         [ 0.04221375,  0.58281521, -1.10061918]]),
+  np.array([[ 0.53035547,  5.94892323,  2.31780174,  3.16005701,  0.53035547],
+         [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ],
+         [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ]]),
+  np.array([[ 0.50249434,  0.90085595],
+         [-0.68372786, -0.12289023],
+         [-0.93576943, -0.26788808]]),
+  np.array([[ 0.40682402,  0.01629284,  0.16722898,  0.10118111,  0.40682402]]),
+  np.array([[-0.6871727 , -0.84520564, -0.67124613]]),
+  )
+    return X_assess, Y_assess, cache
+
+def forward_propagation_with_dropout_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    W1 = np.random.randn(2, 3)
+    b1 = np.random.randn(2, 1)
+    W2 = np.random.randn(3, 2)
+    b2 = np.random.randn(3, 1)
+    W3 = np.random.randn(1, 3)
+    b3 = np.random.randn(1, 1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3}
+    
+    return X_assess, parameters
+
+def backward_propagation_with_dropout_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5)
+    Y_assess = np.array([[1, 1, 0, 1, 0]])
+    # (D1, A1, W1, D2, A2, W2, A3, W3)
+    # (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    cache = (
+           np.array([[ True, False,  True,  True,  True],
+           [ True,  True,  True,  True, False]], dtype=bool), 
+           np.array([[ 0.        ,  0.        ,  4.27989081,  5.21401307,  0.        ],
+           [ 0.        ,  8.32019881,  1.58102041,  2.92987024,  0.        ]]), 
+           np.array([[-1.09989127, -0.17242821, -0.87785842],
+           [ 0.04221375,  0.58281521, -1.10061918]]), 
+           np.array([[ True, False,  True, False,  True],
+           [False,  True, False,  True,  True],
+           [False, False,  True, False, False]], dtype=bool), 
+           np.array([[ 1.06071093,  0.        ,  8.21049603,  0.        ,  1.06071093],
+           [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ],
+           [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ]]), 
+           np.array([[ 0.50249434,  0.90085595],
+           [-0.68372786, -0.12289023],
+           [-0.93576943, -0.26788808]]), 
+           np.array([[ 0.32266394,  0.49683389,  0.00348883,  0.49683389,  0.32266394]]), 
+           np.array([[-0.6871727 , -0.84520564, -0.67124613]]), 
+)
+
+
+    return X_assess, Y_assess, cache
+
